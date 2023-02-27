e5654a15cc0d80f5e8f1b7b08585522896f4e68f
Ke Wang <qqtenxun1234@gmail.com>
2023-02-27 13:19:26 -0800

    Create GPU.sh

---
+++ GPU.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+  
+#SBATCH -t 00:40:00
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=nvlink
+#SBATCH -A aiams
+#SBATCH -p a100
+#SBATCH -J CUGRN
+#SBATCH -o CUGRN_%A_%a.out
+#SBATCH -e CUGRN_%A_%a.err
+ 
+source /etc/profile.d/modules.sh
+module load python/miniconda3.9
+module load cuda/11.1
+
+source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh
+conda activate rapidsai
+
+ulimit -a
+
+export BIN_PATH="$HOME/proj/cugraph"
+export INP_PATH="/people/ghos167/proj/cugraph/inputs/midsize/new"
+
+export CUDA_VISIBLE_DEVICES=0,1
+
+for file in "$INP_PATH"/*
+do
+echo "Processing $file currently..."
+python $BIN_PATH/cug_louvain_mm.py $file
+echo "-----------------------------------"
+echo "-----------------------------------"
+done
